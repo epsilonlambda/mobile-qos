e53b688ae542c766678f71746ca8a4b09cfdc2a8
xiongmao <dev13@xiongmao.(none)>
2016-09-05 02:44:48 -0400

    Poll for disconnection status before reconnecting

--- public/javascripts/panda.js
+++ public/javascripts/panda.js
@@ -1,7 +1,9 @@
 var panda = (function(reconnectionDelay) {
 	var endpoints = {
 		connect: '/api/connect',
-		disconnect: '/api/disconnect'
+		disconnect: '/api/disconnect',
+		isConnected: '/api/is-connected',
+		isDisconnected: '/api/is-disconnected'
 	};
 
 	function changeConnectionState(connected, cb) {
@@ -17,13 +19,32 @@ var panda = (function(reconnectionDelay) {
 		changeConnectionState(false, cb);
 	}
 
-	function reconnect(cb) {
+	function callbackOnPollSuccess(pollEndpoint, interval, cb) {
+		function poller () {
+			$.get(pollEndpoint, function(data) {
+				if(data === true) {
+					cb();
+				} else {
+					setTimeout(poller, interval);
+				}
+			});
+		};
+		
+		return poller;
+	}
+
+	function reconnect(cb, disconnectCb) {
 		disconnect(
 			function() {
-				setTimeout(
+				callbackOnPollSuccess(endpoints.isDisconnected, 2000,
 					function() {
-						connect(cb);
-					}, reconnectionDelay);
+						if(disconnectCb !== undefined)
+							disconnectCb();
+						connect(
+							function() {
+								callbackOnPollSuccess(endpoints.isConnected, 2000, cb)();
+							});
+					})();
 			});
 	}
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var request = require('request');
+var syncRequest = require('sync-request');
+var xml2js = require('xml2js');
 
 function getDialupAPIPayload(shouldConnect) {
   var action = shouldConnect ? 1 : 0;
@@ -13,7 +15,8 @@ var config = {
 };
 
 var endpoints = {
-  dialup: 'http://' + config.host +'/api/dialup/dial'
+  dialup: 'http://' + config.host +'/api/dialup/dial',
+  status: 'http://' + config.host +'/api/monitoring/status'
 };
 
 
@@ -44,6 +47,20 @@ function makeConnectHandler(shouldConnect) {
 router.post('/api/connect', makeConnectHandler(true));
 
 router.post('/api/disconnect', makeConnectHandler(false));
+
+function makeStatusHandler(expectedStatus) {
+	return function(req, res, next) {
+		var hwResponse = syncRequest('GET', endpoints.status);
+		var hwResponseObj = xml2js.parseString(hwResponse.getBody(),
+			function(err, result){
+				console.log(result);
+				res.send(result.response.ConnectionStatus == expectedStatus);
+		});
+
+	};
+}
+router.get('/api/is-disconnected', makeStatusHandler(902));
+router.get('/api/is-connected', makeStatusHandler(901));
 
 
 module.exports = router;
